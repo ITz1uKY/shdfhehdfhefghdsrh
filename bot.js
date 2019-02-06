33572aaa4e229dcdb05e195aec2e9ef6e574749e
ITz1uKY <42788278+ITz1uKY@users.noreply.github.com>
2019-02-06 20:36:29 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const prefix = '=';
+const prefix = 'O';
 
  
  
@@ -91,7 +91,7 @@ if (message.content.startsWith(adminprefix + 'setavatar')) {
 
 
 
-client.login(BOT_TOKEN);
+client.login(process.env.BOT_TOKEN);
 
 
  
